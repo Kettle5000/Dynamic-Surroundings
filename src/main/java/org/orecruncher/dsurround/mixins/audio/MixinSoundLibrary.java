64d05e40231dd972b5a617bc4e2404444e7c0cf9
Astryxion <s.pergolotti@my.ccsu.edu>
2026-05-26 20:29:26 -0400

    fixed sound physics remastered crash

--- src/main/java/org/orecruncher/dsurround/mixins/audio/MixinSoundLibrary.java
+++ src/main/java/org/orecruncher/dsurround/mixins/audio/MixinSoundLibrary.java
@@ -1,22 +1,13 @@
 package org.orecruncher.dsurround.mixins.audio;
 
 import com.mojang.blaze3d.audio.Library;
-import org.lwjgl.openal.ALC10;
-import org.lwjgl.openal.EXTEfx;
-import org.lwjgl.openal.SOFTOutputLimiter;
-import org.lwjgl.system.MemoryStack;
 import org.orecruncher.dsurround.Configuration;
 import org.orecruncher.dsurround.lib.di.ContainerManager;
-import org.orecruncher.dsurround.runtime.audio.AudioUtilities;
 import org.orecruncher.dsurround.mixinutils.ISoundEngine;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
-import java.nio.IntBuffer;
 
 @Mixin(Library.class)
 public class MixinSoundLibrary implements ISoundEngine {
@@ -26,27 +17,6 @@ public class MixinSoundLibrary implements ISoundEngine {
 
     public long dsurround_getDevicePointer() {
         return this.currentDevice;
-    }
-
-    @Redirect(
-            method = "init(Ljava/lang/String;Z)V",
-            at = @At(value = "INVOKE", target = "Lorg/lwjgl/openal/ALC10;alcCreateContext(JLjava/nio/IntBuffer;)J", remap = false)
-    )
-    private long dsurround_createContext(long deviceHandle, IntBuffer attrList) {
-        if (AudioUtilities.doEnhancedSounds()) {
-            try (MemoryStack memoryStack = MemoryStack.stackPush()) {
-                IntBuffer buffer = memoryStack.callocInt(5)
-                        .put(SOFTOutputLimiter.ALC_OUTPUT_LIMITER_SOFT)
-                        .put(ALC10.ALC_TRUE)
-                        .put(EXTEfx.ALC_MAX_AUXILIARY_SENDS)
-                        .put(4)
-                        .put(0)
-                        .flip();
-                return ALC10.alcCreateContext(deviceHandle, buffer);
-            }
-        }
-
-        return ALC10.alcCreateContext(deviceHandle, attrList);
     }
 
     /**
